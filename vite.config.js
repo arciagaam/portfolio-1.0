edf67e876577a02f08e74c51ef92c451572c9fc0
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-04-14 03:54:04 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/arciagaam.github.io/portfolio-1.0/',
+  base:'https://arciagaam.github.io/portfolio-1.0/',
   plugins: [react()],
 })
