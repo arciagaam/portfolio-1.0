da0a4efa8f14ec03889756b4523d952b19a926a4
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-04-14 10:05:13 +0800

    fixed icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useLayoutEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './pages/Home'
 import { gsap } from "gsap";
 function App() {
